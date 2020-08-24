2649c0fbd07b795fd00b6cc3f9ac81ec38751799
Ngo Hoai Than <than1089@gmail.com>
2020-08-24 13:48:36 +0700

    Updates

---
+++ precache-manifest.f82f983fe74b2477b704fe017bbac996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0043bd70e8605929d219b23eaa01c776",
+    "url": "/react-photo-album/index.html"
+  },
+  {
+    "revision": "a3198361e33437cb3c8a",
+    "url": "/react-photo-album/static/css/main.3fa50a71.chunk.css"
+  },
+  {
+    "revision": "fae16f9d40747483cc2d",
+    "url": "/react-photo-album/static/js/2.26cfd270.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-photo-album/static/js/2.26cfd270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3198361e33437cb3c8a",
+    "url": "/react-photo-album/static/js/main.0526de71.chunk.js"
+  },
+  {
+    "revision": "f224d8e49953171f2da9",
+    "url": "/react-photo-album/static/js/runtime-main.8bcb751d.js"
+  }
+]);
